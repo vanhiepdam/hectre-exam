43243941f8f16aba897902d6d766d9d5ab7bdf7a
Dam Hiep <hiep@kiuglobal.com>
2022-10-11 17:34:23 +0700

    add Dockerfile

--- harvest_be/source/harvest_be/settings/base.py
+++ harvest_be/source/harvest_be/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dotenv
 
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b%*xx7(d#f^4cizyp9bay8_3xz)&8&r_ow$j33obs321@^@9l(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
