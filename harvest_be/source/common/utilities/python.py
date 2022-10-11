3efd67ff19277d3d2353bd33261e723dadc70950
Dam Hiep <hiep@kiuglobal.com>
2022-10-12 00:29:12 +0700

    make fail

--- harvest_be/source/common/utilities/python.py
+++ harvest_be/source/common/utilities/python.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-
-class PythonUtil:
+class PythonUtil
     @classmethod
     def get_class_full_import_path(cls, _class):
         return _class.__module__ + '.' + _class.__name__
